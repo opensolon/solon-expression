40d03cc2a9acaf73ff66cc8815688984775f235e
noear <noear@live.cn>
2025-06-30 17:05:30 +0800

    优化 solon-expression tmpl 优化非模板性能

--- solon-expression-test/src/test/java/benchmark/expr/TmplTest.java
+++ solon-expression-test/src/test/java/benchmark/expr/TmplTest.java
@@ -4,6 +4,7 @@ import org.noear.solon.core.util.TmplUtil;
 import org.noear.solon.expression.context.StandardContext;
 import org.noear.solon.expression.snel.SnEL;
 
+import java.util.HashMap;
 import java.util.Map;
 import java.util.Properties;
 
@@ -12,33 +13,50 @@ import java.util.Properties;
  */
 public class TmplTest {
     public static void main(String[] args) {
+        case1();
+        System.out.println("/////////////////////////");
+        case2();
+    }
+
+    private static void case1() {
         Properties props = new Properties();
         props.setProperty("user", "solon");
         props.setProperty("lang", "java");
 
         int count = 10_000_000;
-        case1(count, "user", "user", props);
-        case1(count, "${user}", "${user}", props);
-        case1(count, "${user}_${lang}", "${user}_${lang}", props);
-        case1(count, "${user}_${lang}_${lang}", "${user}_${lang}_${lang}", props);
+        execDo(count, "user", "user", props);
+        execDo(count, "${user}", "#{user}", props);
+        execDo(count, "${user}_${lang}", "#{user}_#{lang}", props);
+        execDo(count, "${user}_${lang}_${lang}", "#{user}_#{lang}_#{lang}", props);
     }
 
-    private static void case1(int count, String tmpl, String tmpl2, Properties props) {
+    private static void case2() {
+        Map<String, Object> model = new HashMap<>();
+        model.put("user", "noear");
+        model.put("label", 1);
+        model.put("", model);
+
+        int count = 10_000_000;
+        execDo(count, "user=${user}", "user=#{user}", model);
+    }
+
+
+    private static void execDo(int count, String tmpl, String tmpl2, Map props) {
         System.out.println("----------------------------");
         System.out.println("tmpl: " + tmpl);
         System.out.println("----------------------------");
 
-        System.out.println("TmplUtil:" + (TmplUtil.parse(tmpl, (Map) props)));
+        System.out.println("TmplUtil:" + (TmplUtil.parse(tmpl, props)));
         System.out.println("SnEL:" + (SnEL.evalTmpl(tmpl2, new StandardContext(props))));
 
         for (int i = 0; i < 10; i++) {
-            TmplUtil.parse(tmpl, (Map) props);
+            TmplUtil.parse(tmpl, props);
             SnEL.evalTmpl(tmpl2, new StandardContext(props));
         }
 
         long start = System.currentTimeMillis();
         for (int i = 0; i < count; i++) {
-            TmplUtil.parse(tmpl, (Map) props);
+            TmplUtil.parse(tmpl, props);
         }
         long span = System.currentTimeMillis() - start;
         System.out.println("TmplUtil:" + span);
